0c4a4291b013587025225e3b8dae5e21194b429d
wemali05 <alfredwemali05@gmail.com>
2020-02-27 17:23:30 +0300

    user factory fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,7 @@ $factory->define(User::class, function (Faker $faker) {
         'phone' =>$faker->phoneNumber,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'profile_image' => 'https://via.placeholder.com/150*150',
+        'profile_image' => 'https://via.placeholder.com/150',
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];

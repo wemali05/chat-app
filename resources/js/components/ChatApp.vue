8cf4489586ef7e36823436546143c0278e281af0
wemali05 <alfredwemali05@gmail.com>
2020-02-27 17:24:17 +0300

    chapapp component

---
+++ resources/js/components/ChatApp.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="chat-app">
+      <Converstion :contact="selectedContact" :messages="messages"/>
+      <ContactsList :contacts="contacts"/>
+    </div>
+</template>
+
+<script>
+import  Converstion from './Converstion';
+import  ContactsList from './ContactsList';
+
+    export default {
+        props: {
+            user: {
+                type: Object,
+                required: true
+            }
+        },
+        data() {
+            return {
+                selectedContact: null,
+                messages: [],
+                contacts: []
+            }
+        },
+        mounted() {
+            console.log(this.user);
+            axios.get('/contacts')
+            .then((response) => {
+                console.log(response.data);
+                this.contacts = response.data;
+            });
+        },
+
+        components: { Converstion, ContactsList }
+    }
+</script>

ef171782876dcb7272a48f2e6f5f21424c5e7450
wemali05 <alfredwemali05@gmail.com>
2020-02-27 17:23:10 +0300

    contacts controller

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class ContactsController extends Controller
+{
+    public function get()
+    {
+        $contacts = User::all();
+
+        return response()->json($contacts);
+    }
+}

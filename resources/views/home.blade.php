83cca2a120e6c6cc1206ba26693b33532c780745
wemali05 <alfredwemali05@gmail.com>
2020-02-27 17:24:53 +0300

    render page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Messanger</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+                <div class="card-body" id="app">
+                    <div>
+                        <chat-app :user="{{ auth()->user() }}"></chat-app>
+                    </div>
+                  </div>
             </div>
         </div>
     </div>

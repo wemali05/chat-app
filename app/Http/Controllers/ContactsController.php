540e1e6e007aef1a39043e3f66030f4a229a9db1
wemali05 <alfredwemali05@gmail.com>
2020-02-28 13:55:56 +0300

    reduce message results

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use App\Message;
- use App\Events\NewMessege;
+use App\Events\NewMessege;
 
 
 use Illuminate\Http\Request;
@@ -22,6 +22,17 @@ class ContactsController extends Controller
     {
         $messeges = Message::where('from', $id)->orWhere('to', $id)->get();
 
+        // get all messages between the authenticated user and the selected user
+        $messeges = Message::where(function ($q) use ($id) {
+            $q->where('from', auth()->id());
+            $q->where('to', $id);
+        })->orWhere(function ($q) use ($id) {
+            $q->where('from', $id);
+            $q->where('to', auth()->id());
+        })
+        ->get();
+
+
         return response()->json($messeges);
     }
 
@@ -33,8 +44,8 @@ class ContactsController extends Controller
             'text' => $request->text
             ]);
 
-            broadcast(new NewMessege($messege));
+        broadcast(new NewMessege($messege));
 
-            return response()->json($messege);
+        return response()->json($messege);
     }
 }

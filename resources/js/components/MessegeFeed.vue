a63c8f09a7614aa1158e3d060d6df432f69a682d
wemali05 <alfredwemali05@gmail.com>
2020-02-28 11:02:52 +0300

    scroll bottom

--- resources/js/components/MessegeFeed.vue
+++ resources/js/components/MessegeFeed.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="feed">
+    <div class="feed" ref="feed">
         <ul v-if="contact">
              <li v-for="message in messages" :class="`message${message.to == contact.id ? ' sent' : ' received'}`" :key="message.id">
                 <div class="text">
@@ -20,7 +20,24 @@ export default {
              type: Array,
              required: true
          }
-     }
+     },
+     
+     methods: {
+         scrollToBottom(){
+             setTimeout(() => {
+                    this.$refs.feed.scrollTop = this.$refs.feed.scrollHeight - this.$refs.feed.clientHeight;
+             }, 50);
+           }
+     },
+
+     watch: {
+         contact(contact){
+             this.scrollToBottom();
+         },
+        messages(messages){
+             this.scrollToBottom();
+        }
+     },
 }
 </script>
 
